58b4a265ceabe2e5f068cd45194b45e1b2e46ff4
unknown <you@example.com>
2021-12-31 09:52:31 +0600

    updated

---
+++ pages/blog/first.js
@@ -0,0 +1,9 @@
+function Frist() {
+  return (
+    <div>
+      <h2>This is Naimur Rahmans</h2>
+    </div>
+  );
+}
+
+export default Frist;
---
+++ pages/blog/secound.js
@@ -0,0 +1,7 @@
+function Secound() {
+  return (
+    <div>
+      <h2>This is Secound</h2>
+    </div>
+  );
+}
